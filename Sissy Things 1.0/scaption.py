81a49dd1088407a24e8db4f824bb45cf77b0cc0b
blacksnowmartin <martinmwendwa005@gmail.com>
2025-05-12 01:01:48 +0300

    added a new segment

---
+++ Sissy Things 1.0/scaption.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Create a figure and axis
+fig, ax = plt.subplots()
+
+# Set the background color
+ax.set_facecolor('white')
+
+# Add text to the plot
+ax.text(0.5, 0.5, 'Sissy Caption', fontsize=50, color='pink', ha='center', va='center')
+
+# Remove axes
+ax.axis('off')
+
+# Show the plot
+plt.show()
