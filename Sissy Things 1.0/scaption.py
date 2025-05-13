f8bac62f7d8e98d30322ec725e679040d36035bf
blacksnowmartin <martinmwendwa005@gmail.com>
2025-05-13 09:30:44 +0300

    added a caption

--- Sissy Things 1.0/scaption.py
+++ Sissy Things 1.0/scaption.py
@@ -7,7 +7,7 @@ fig, ax = plt.subplots()
 ax.set_facecolor('white')
 
 # Add text to the plot
-ax.text(0.5, 0.5, 'Sissy Caption', fontsize=50, color='pink', ha='center', va='center')
+ax.text(0.5, 0.5, 'I am a Sissy', fontsize=50, color='pink', ha='center', va='center')
 
 # Remove axes
 ax.axis('off')
